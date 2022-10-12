c5db5cb532fa96e2d9a1270258f9a3b0b4d07c8c
kitts <kittsmakokha@gmail.com>
2022-10-12 06:25:19 +0000

    update

--- string1.c
+++ string1.c
@@ -1,12 +1,12 @@
 #include "shell.h"
 
 /**
-  * _strcpy - copies a string
-  * @dest: the destination
-  * @src: the source
-  *
-  * Return: pointer to destination
-  */
+ * _strcpy - copies a string
+ * @dest: the destination
+ * @src: the source
+ *
+ * Return: pointer to destination
+ */
 char *_strcpy(char *dest, char *src)
 {
 	int i = 0;
@@ -23,11 +23,11 @@ char *_strcpy(char *dest, char *src)
 }
 
 /**
-  * _strdup - duplicates a string
-  * @str: the string to duplicate
-  *
-  * Return: pointer to the duplicated string
-  */
+ * _strdup - duplicates a string
+ * @str: the string to duplicate
+ *
+ * Return: pointer to the duplicated string
+ */
 char *_strdup(const char *str)
 {
 	int length = 0;
@@ -46,11 +46,11 @@ char *_strdup(const char *str)
 }
 
 /**
-  *_puts - prints an input string
-  *@str: the string to be printed
-  *
-  * Return: Nothing
-  */
+ *_puts - prints an input string
+ *@str: the string to be printed
+ *
+ * Return: Nothing
+ */
 void _puts(char *str)
 {
 	int i = 0;
@@ -65,12 +65,12 @@ void _puts(char *str)
 }
 
 /**
-  * _putchar - writes the character c to stdout
-  * @c: The character to print
-  *
-  * Return: On success 1.
-  * On error, -1 is returned, and errno is set appropriately.
-  */
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
 int _putchar(char c)
 {
 	static int i;
